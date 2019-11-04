0cd83413928fd45fc50ddd0912cc5ffde6feef89
Kristiana Blake <kristiana.blake@gmail.com>
2019-11-03 21:33:46 -0600

    server.js file created

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-require('./db/db')
+require('./db/db');
+
+app.use(methodOverride('_method'));//must come before our routes
+app.use(bodyParser.urlencoded({extended: false}));
+
+const userController = require('./controllers/review.js')
+app.use('/review', reviewController);
+
+const userController = require('./controllers/user.js');
+app.use('/user', userController);
+
+// home page
+app.get('/',(req, res) => {
+  res.render('home.ejs')
+});
 
 
 app.listen(3000, () => {
